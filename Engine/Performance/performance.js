37a00a3879f348c878987b74bdb3893272adeb10
SeHyun <rla6170@naver.com>
2024-06-26 16:18:39 +0900

    0626 : javascript performance test

---
+++ Engine/Performance/performance.js
@@ -0,0 +1,8 @@
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+
+const c = [...a, ...b];
+// Use Spread Performance is better length 10
+
+const d = a.concat(b);
+// use concat method Performance is better length 100
